3752a560fa5a0c4dfa25daf8958d4bdbd418efdf
Vitor Nascimento <bielvitor2008@hotmail.com>
2021-03-22 18:35:17 -0300

    Vincula dados na view.

---
+++ app/src/main/java/me/vitornascimento/todomovies/model/Genre.kt
@@ -0,0 +1,6 @@
+package me.vitornascimento.todomovies.model
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
--- app/src/main/java/me/vitornascimento/todomovies/service/TmdbService.kt
+++ app/src/main/java/me/vitornascimento/todomovies/service/TmdbService.kt
@@ -2,6 +2,7 @@ package me.vitornascimento.todomovies.service
 
 import me.vitornascimento.todomovies.BuildConfig
 import me.vitornascimento.todomovies.model.Movie
+import me.vitornascimento.todomovies.model.SimilarMoviesResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -11,9 +12,16 @@ interface TmdbService {
 
     @GET("movie/{movie_id}")
     suspend fun getMovie(
-            @Path("movie_id") movie_id: Int,
-            @Query("api_key") api_key: String = BuildConfig.API_KEY,
-            @Query("language") language: String = "pt-BR"
+        @Path("movie_id") movie_id: Int,
+        @Query("api_key") api_key: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "pt-BR"
     ): Response<Movie>
+
+    @GET("movie/{movie_id}/similar")
+    suspend fun getSimilarMovies(
+        @Path("movie_id") movie_id: Int,
+        @Query("api_key") api_key: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "pt-BR"
+    ): Response<SimilarMoviesResponse>
 
 }
